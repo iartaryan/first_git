3187cf4f4c61604543002e117a158a59fc418fc7
iartaryan <i.artaryan@gmail.com>
2017-09-02 17:35:29 +0300

    Delete answers.py

--- answers.py
+++
@@ -1,5 +0,0 @@
-def get_answer(key):
-	answer = {'привет':'И тебе привет','Как дела?':'Лучше всех!', 'Пока':'Увидимся'}
-	return answer.get(key, 'Не знаю о чем ты')
-
-print(get_answer(input('Введите что-нибудь: ').lower()))
